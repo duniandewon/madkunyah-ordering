2075321d64002aecb9e832261bb0027c68f44527
ndewon <duniandewon@gmail.com>
2026-06-01 00:10:56 +0700

    Setup router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/screens/Home.vue"),
+    },
+    {
+      path: "/menu/:id",
+      component: () => import("@/screens/MenuDetail.vue"),
+    },
+    {
+      path: "/cart",
+      component: () => import("@/screens/Cart.vue"),
+    },
+    {
+      path: "/payment/:id",
+      component: () => import("@/screens/Payment.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
